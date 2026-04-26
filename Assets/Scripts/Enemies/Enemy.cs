f5c4bf6d25cda362faed4e07ae781897c3918070
ChristopherTGreen <ctgreen00@gmail.com>
2026-04-25 22:10:11 -0700

    Enemy Class
    - Created enemy class for storage with serialization (double check if this is needed)

---
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Enemy
+// Stores the information or stats of each enemy, each in a dictionary it references to
+[Serializable]
+public class Enemy
+{
+    public string name;
+    // Making sure, do we need sprite name or index (i'm going with index for memory reasons) (clarify me if I am wrong) - Chris
+    public int sprite;
+    public int health;
+    public int speed;
+    public int damage;
+    
+    // surely there must be a better way to store this information with keeping encapsulation?
+
+
+}
