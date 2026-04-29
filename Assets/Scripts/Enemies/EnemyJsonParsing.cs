141058577e1d5fc884f0be52f5f69c9e24f41ebc
Jay Chung <tlchung@ucsc.edu>
2026-04-28 22:01:26 -0700

    JsonProcessingTemplate + EnemyjsonParsing
    - Reworked the Json template and the EnemyJsonParsing (Child)
    - Referenced: https://yawgmoth.github.io/CMPM121/slides/lecture4.html#15

--- Assets/Scripts/Enemies/EnemyJsonParsing.cs
+++ Assets/Scripts/Enemies/EnemyJsonParsing.cs
@@ -1,20 +1,36 @@
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System.Collections.Generic;
 
 public class EnemyParsing : JsonProcessingTemplate<EnemyData>
 {
-    
-    protected override EnemyData ParseData(string jsonfile)
+
+    // The dictionary parameter is provided in the StoreData method.
+    // To give the json file, you need to use Resources.Load<TextAsset>("enemies").text (method from Newtonsoft.Json;) as a parameter.
+    protected override void ParseData(string jsonfile, Dictionary<string, EnemyData> dictionary)
     {
         
+        JToken jsonObject = JToken.Parse(jsonfile); //gets all of the objects from the Json file
+
+        //Parses each object and "uploads" them to the dictionary with the enemy name as the key and the EnemyData type as the value
+        foreach (var enemy in jsonObject)
+        {
+            EnemyData enemyData = enemy.ToObject<EnemyData>(); 
+            dictionary[enemyData.name] = enemyData;
+        }
+
     }
 
-    private void AddEnemyData(Dictionary<string, EnemyData> dictionaryReference)
+}
+
+//This should be it's own class in this file
+
+/*
+private void AddEnemyData(Dictionary<string, EnemyData> dictionaryReference)
     {
         EnemyParsing enemy = new EnemyParsing();
 
         EnemyData newEnemy = enemy.StoreData("enemies.json");
         dictionaryReference.Add(newEnemy.name, newEnemy);
     }
-
-}
+*/
--- Assets/Scripts/JsonProcessing/JsonProcessingTemplate.cs
+++ Assets/Scripts/JsonProcessing/JsonProcessingTemplate.cs
@@ -3,29 +3,21 @@ using System;
 using System.IO;
 using Newtonsoft.Json.Linq;
 
-//Template Method Pattern?
+//Template Method Pattern
 public abstract class JsonProcessingTemplate<Type>
 {
 
-    private Dictionary<string, Type> StoreData(string jsonfile)
+    public Dictionary<string, Type> StoreData(string jsonfile)
     {
         
-        Dictionary<string, Type> StoredData = new Dictionary<string, Type>();
-        ParseData(jsonfile, StoredData);
-        return StoredData;
+        // StoreData will return a dictionary with the json data stored in them
+        Dictionary<string, Type> StoredData = new Dictionary<string, Type>(); //The 'Type' will be a type class that we define (Ex: EnemyData defined in EnemyData.cs)
+        ParseData(jsonfile, StoredData); // This method will be overided depending on which json file we're parsing
+        return StoredData; // Returned dictionary that has the jsondata stored in them
+
     }
 
     //Children will override this method
     protected abstract void ParseData(string jsonfile, Dictionary<string, Type> dictionary);
 
-    /*
-    protected string ReadJsonFile(string jsonfilename)
-    {
-
-        return File.ReadAllText(jsonfilename);
-
-    }
-
-    protected abstract Type ParseJsonFile(string data); //Children using this template will edit this method
-    */
 }
