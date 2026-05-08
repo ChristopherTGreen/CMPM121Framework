2addce664b0a0ea2388b1f5761a0c70fd8de559f
Jay Chung <tlchung@ucsc.edu>
2026-05-07 20:28:14 -0700

    More SpellJsonParsing [Not Done]

--- Assets/Scripts/Spells/SpellJsonParsing.cs
+++ Assets/Scripts/Spells/SpellJsonParsing.cs
@@ -1,13 +1,21 @@
-using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System.Collections.Generic;
-using System.Text.Json;
+
 
 public class SpellParsing : JsonProcessingTemplate<SpellData>
 {
     
     protected override void ParseData(string jsonfile, Dictionary<string, SpellData> dictionary)
     {
-        JToken jsonObject = JToken.Parse(jsonfile);
+
+        JToken jsonObject = JToken.Parse(jsonfile); //gets all of the objects from the Json file
+
+        foreach (var level in jsonObject)
+        {
+            SpellData spellData = level.ToObject<SpellData>();
+            dictionary[spellData.name] = spellData;
+        }
+
     }
 
 }
