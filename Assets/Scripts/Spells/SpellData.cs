15fcd4d980cf7f29bcb3785acad9fab89e4d6af9
Jay Chung <tlchung@ucsc.edu>
2026-05-11 00:28:54 -0700

    Spell Data PArsing Fix?

--- Assets/Scripts/Spells/SpellData.cs
+++ Assets/Scripts/Spells/SpellData.cs
@@ -2,24 +2,17 @@ using System.Collections.Generic;
 using Newtonsoft.Json;
 using UnityEngine;
 
+
 public class SpellData
-{
-    
-    public List<SpellPropertiesData> spell = new List<SpellPropertiesData>();
-
-}
-
-public class SpellPropertiesData
 {
     
     public string name { get ; set;} = null;
     public string description { get ; set; } = null;
     public int icon { get ; set; } = -1;
-    public List<SpellDamageData> damage = new List<SpellDamageData>();
-    public int mana_cost { get ; set; } = -1;
-    public int cooldown { get ; set; } = -1;
-    public List<SpellProjectileData> projectile = new List<SpellProjectileData>();
-
+    public SpellDamageData damage { get ; set; } = null;
+    public string mana_cost { get ; set; } = null;
+    public string cooldown { get ; set; } = null;
+    public SpellProjectileData projectile { get ; set; } = null;
 }
 
 public class SpellDamageData
