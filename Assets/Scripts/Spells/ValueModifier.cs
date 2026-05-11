87421765e12ecdf72cd7abf97063b1da13d5848c
ChristopherTGreen <ctgreen00@gmail.com>
2026-05-10 23:59:31 -0700

    Updated value modifier
    - added mul operation
    - Fixed the get value, not sure about the list

--- Assets/Scripts/Spells/ValueModifier.cs
+++ Assets/Scripts/Spells/ValueModifier.cs
@@ -9,10 +9,16 @@ public class ValueModifier<T>
     {
         return original;
     }
+    public static T GetValue(List<ValueModifier<T>> mods, T original)
+    {
+        // I believe it returns a list of values, defined by the mods
+        foreach (ValueModifier<T> mod in mods)
+        {
+            return original;
+        }
+    }
 }
-public class ConstantAdderModifierGeneric<Ops, T> :
-        ValueModifier<T>
-        where Ops : MathOperations<T>, new()
+public class ConstantAdderModifierGeneric<Ops, T> : ValueModifier<T> where Ops : MathOperations<T>, new()
 {
     public T add;
     public ConstantAdderModifierGeneric(T add)
@@ -24,10 +30,25 @@ public class ConstantAdderModifierGeneric<Ops, T> :
         return new Ops().Add(original, add);
     }
 }
+public class ConstantMultiplierModifierGeneric<Ops, T> :
+        ValueModifier<T>
+        where Ops : MathOperations<T>, new()
+{
+    public T mul;
+    public ConstantMultiplierModifierGeneric(T mul)
+    {
+        this.mul = mul;
+    }
+    public override T GetValue(T original)
+    {
+        return new Ops().Mul(original, mul);
+    }
+}
 
 public interface MathOperations<T>
 {
     T Add(T a, T b);
+    T Mul(T a, T b);
     // and other operations
 }
 public class MathOperationsInt :
@@ -36,5 +57,9 @@ public class MathOperationsInt :
     public int Add(int a, int b)
     {
         return a + b;
+    }
+    public int Mul(int a, int b)
+    {
+        return a * b;
     }
 }
