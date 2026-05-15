4bd6dcddeada944c91e333f28a82311f09f0babe
ChristopherTGreen <ctgreen00@gmail.com>
2026-05-15 01:17:25 -0700

    Class Data
    - Implemented JSON parsing for class data
    - Created Class Data for future storage of classes

---
+++ Assets/Scripts/Classes/ClassData.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+using UnityEngine;
+
+
+
+public class ClassData
+{
+
+    public string name { get; set; } = null;
+    public int sprite { get; set; } = 0;//Sprites are indexed - Jay
+    public string health { get; set; } = null;
+    public string mana { get; set; } = null;
+    public string mana_regeneration { get; set; } = null;
+    public string spellpower { get; set; } = null;
+    public string speed { get; set; } = null;
+
+    [JsonConstructor]
+    public ClassData(string initName, int initSprite, string initHealth, string initMana, string initManaRegen, string initSpellPower, string initSpeed)
+    {
+
+        name = initName;
+        sprite = initSprite;
+        health = initHealth;
+        mana = initMana;
+        mana_regeneration = initManaRegen;
+        spellpower = initSpellPower;
+        speed = initSpeed;
+
+    }
+
+}
+
+
+
+// call this class to retrieve the dictionary with all the enemy data
+public class RetrieveClassData
+{
+
+    private static ClassParsing ClassParse = new ClassParsing();
+    private static Dictionary<string, ClassData> classdictionary = new Dictionary<string, ClassData>();
+
+    // Programmer can now call Dictionary<string, EnemyData> enemy_dictionary = RetrieveEnemyData.EnemyDictionary();
+    public static Dictionary<string, ClassData> EnemyDictionary()
+    {
+
+        classdictionary = ClassParse.StoreData(Resources.Load<TextAsset>("classes").text);
+        return classdictionary;
+
+    }
+
+}
