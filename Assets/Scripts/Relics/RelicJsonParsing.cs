73209b5b31d62284dab08963e0388c29ba4b849f
Jay Chung <tlchung@ucsc.edu>
2026-05-18 02:59:27 -0700

    Relic Json Parsing - NOt DOne

--- Assets/Scripts/Relics/RelicJsonParsing.cs
+++ Assets/Scripts/Relics/RelicJsonParsing.cs
@@ -1,1 +1,54 @@
 // Note: File created by Jay for assignment 4 - Not provided by the professor
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.Linq;
+
+
+// Might need to rework this. I'm pretty sure we're going to have a parsing error since the Newtonsoft library 
+// will throw a error if it can't find enough variables to store each property - the splitting of the triggers
+// and effects makes this a bit difficult to make.
+
+
+// parses the relic triggers data
+public class RelicTriggersParsing : JsonProcessingTemplate<RelicTriggersData>
+{
+
+    protected override void ParseData(string jsonfile, Dictionary<string, RelicTriggersData> dictionary)
+    {
+
+        JObject jsonObject = JObject.Parse(jsonfile); //gets all of the objects from the Json file
+        
+        foreach (var relicType in jsonObject.Properties())
+        {
+
+            RelicTriggersData relicTriggersData = relicType.Value.ToObject<RelicTriggersData>();
+            dictionary[relicTriggersData.name] = relicTriggersData;
+
+        }
+
+    }
+
+}
+
+
+
+// Parses the relic events data
+public class RelicEventsParsing : JsonProcessingTemplate<RelicEventsData>
+{
+
+    protected override void ParseData(string jsonfile, Dictionary<string, RelicEventsData> dictionary)
+    {
+
+        JObject jsonObject = JObject.Parse(jsonfile); //gets all of the objects from the Json file
+        
+        foreach (var relicType in jsonObject.Properties())
+        {
+
+            RelicEventsData relicEventsData = relicType.Value.ToObject<RelicEventsData>();
+            dictionary[relicEventsData.name] = relicEventsData;
+
+        }
+
+    }
+
+}
