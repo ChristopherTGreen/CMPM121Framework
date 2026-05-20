747f9865610c0572f1e0f5490094333adb4b9ad8
Jay Chung <tlchung@ucsc.edu>
2026-05-20 15:08:02 -0700

    Starting the Relic Reward

---
+++ Assets/Scripts/UI/RewardRelic.cs
@@ -0,0 +1,35 @@
+// this script should handle the display of the collection buttons, description and icons 
+using UnityEngine;
+using System.Collections.Generic;
+using TMPro;
+
+
+
+public class RewardRelicDisplay
+{
+    
+    // Unity inspector stuff - assign in there
+    public GameObject relicPrefab;
+    public GameObject buttonPrefab;
+    public TextMeshProUGUI relicDescription;
+
+    void Start()
+    {
+        
+    }
+
+    public void DisplayRelicRewards()
+    {
+        // get a list of three randomly selected relics
+
+        // Instantiate the button and all the displays
+
+        // Add listeners to each button
+    }
+
+    public void TakeRelicHandler()
+    {
+        //handles taking a relic and blocks off the other buttons from being clicked.
+    }
+
+}
