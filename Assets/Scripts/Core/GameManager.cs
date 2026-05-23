6469839eb7c66a80486671ca176cff33306fb939
Jay Chung <tlchung@ucsc.edu>
2026-05-22 18:07:06 -0700

    Conflict resolved

--- Assets/Scripts/Core/GameManager.cs
+++ Assets/Scripts/Core/GameManager.cs
@@ -121,12 +121,5 @@ public class GameManager
         classTypes = RetrieveClassData.ClassDictionary();
         relics = RetrieveRelicData.RelicsDictionary();
         Debug.Log(string.Join(", ", relics));
-<<<<<<< Updated upstream
-=======
-
-        // temporary hard code
-        activeRelics.Add("Green Gem", new GreenGem(relics["Green Gem"]));
-        activeRelics["Green Gem"].Enable();
->>>>>>> Stashed changes
     }
 }
