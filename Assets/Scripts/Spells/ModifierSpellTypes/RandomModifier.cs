b2d42ecec2972ea492bedb05bf593dfaa785a2c2
ChristopherTGreen <ctgreen00@gmail.com>
2026-06-02 22:41:05 -0700

    Arcane Spray
    - Added Arcane Spray spell
    - Added all current modifiers as options in randomizer

--- Assets/Scripts/Spells/ModifierSpellTypes/RandomModifier.cs
+++ Assets/Scripts/Spells/ModifierSpellTypes/RandomModifier.cs
@@ -9,7 +9,9 @@ public class RandomModifier
     private Dictionary<string, Func<SpellCaster, Spell>> spellDict = new Dictionary<string, Func<SpellCaster, Spell>>
     {
         { "ArcaneBolt", (owner) => new ArcaneBolt(owner) },
-        { "MagicMissile", (owner) => new MagicMissile(owner) }
+        { "MagicMissile", (owner) => new MagicMissile(owner) },
+        { "ArcaneBlast", (owner) => new ArcaneBlast(owner) },
+        { "ArcaneSpray", (owner) => new ArcaneSpray(owner) }
     };
     private Dictionary<string, Func<Spell, SpellModifier>> modDict = new Dictionary<string, Func<Spell, SpellModifier>>
     {
