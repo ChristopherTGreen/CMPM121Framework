9e694c42f072749c6e96b95a459b20d5067f6acf
Robert Stager <queenbee.stager@gmail.com>
2026-06-08 14:30:38 -0700

    Starting on the RPG Stats
    Make a public enumerator for the stats and started on class for handling the upgrades

---
+++ Assets/Scripts/RPGStats/PlayerStatType.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    public enum PlayerStatType
+    {
+        MaxHealth,
+        SpellPower,
+        ManaRegen,
+        MaxMana,
+        MoveSpeed
+    }
+}
